fd5ce7f58fe338ab0b2444a0ce4a320ebce1d866
Maria Predchenko <126472968+mariumi-ma@users.noreply.github.com>
2025-08-23 17:12:27 +0300

    добавлен модуль app

--- 4-order-api/cmd/main.go
+++ 4-order-api/cmd/main.go
@@ -4,61 +4,18 @@ import (
 	"log"
 	"net/http"
 
-	"purpleschool/configs"
-	"purpleschool/internal/auth"
-	"purpleschool/internal/database"
-	"purpleschool/internal/logger"
-	"purpleschool/internal/middleware"
-	"purpleschool/internal/order"
-	"purpleschool/internal/product"
-	"purpleschool/internal/user"
+	"purpleschool/internal/app"
 )
 
 func main() {
-	config, err := configs.LoadConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log := logger.NewLogger()
-
-	db, err := database.NewDB(config)
-	if err != nil {
-		log.Fatal("Ошибка при подключении к базе данных", err)
-	}
-
-	router := http.NewServeMux()
-
-	jwt := auth.NewJWT(config.Token.SecretKey)
-
-	// Repositories
-	productRepository := product.NewProductRepository(db)
-	userRepository := user.NewUserRepository(db)
-	orderRepository := order.NewOrderRepository(db)
-
-	// Services
-	authService := auth.NewAuthService(userRepository)
-
-	// Middleware
-	mw := middleware.NewMiddleware(log, jwt)
-	stack := mw.Chain(
-		mw.SetRequestID,
-		mw.RecoverPanic,
-		mw.CORS,
-		mw.Logging,
-	)
-
-	// Handlers
-	auth.NewAuthHandler(router, config, authService)
-	product.NewProductHandler(router, productRepository)
-	order.NewOrderHandler(router, orderRepository, mw)
+	app := app.App()
 
 	server := http.Server{
 		Addr:    ":8081",
-		Handler: stack(router),
+		Handler: app,
 	}
 
-	log.Info("Server started on port 8080")
+	log.Println("Server started on port 8080")
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
---
+++ 4-order-api/internal/app/app.go
@@ -0,0 +1,57 @@
+package app
+
+import (
+	"log"
+	"net/http"
+
+	"purpleschool/configs"
+	"purpleschool/internal/auth"
+	"purpleschool/internal/database"
+	"purpleschool/internal/logger"
+	"purpleschool/internal/middleware"
+	"purpleschool/internal/order"
+	"purpleschool/internal/product"
+	"purpleschool/internal/user"
+)
+
+func App() http.Handler {
+	config, err := configs.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log := logger.NewLogger()
+
+	db, err := database.NewDB(config)
+	if err != nil {
+		log.Fatal("Ошибка при подключении к базе данных", err)
+	}
+
+	router := http.NewServeMux()
+
+	jwt := auth.NewJWT(config.Token.SecretKey)
+
+	// Repositories
+	productRepository := product.NewProductRepository(db)
+	userRepository := user.NewUserRepository(db)
+	orderRepository := order.NewOrderRepository(db)
+
+	// Services
+	authService := auth.NewAuthService(userRepository)
+
+	// Middleware
+	mw := middleware.NewMiddleware(log, jwt)
+	stack := mw.Chain(
+		mw.SetRequestID,
+		mw.RecoverPanic,
+		mw.CORS,
+		mw.Logging,
+	)
+
+	// Handlers
+	auth.NewAuthHandler(router, config, authService)
+	product.NewProductHandler(router, productRepository)
+	order.NewOrderHandler(router, orderRepository, mw)
+
+	return stack(router)
+}
